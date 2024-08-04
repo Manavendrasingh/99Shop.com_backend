41aa2eec6efd2ec8a84d4fe458c7142b09c20544
manavendrajss@gmail.com <manavendrajss@gmail.com>
2024-08-04 11:47:59 +0530

    cors error update commit

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ import express from "express";
  import dbConnect from "./dbConntect.js";
  import userRouter from "./Routers/users.Router.js";
  import cookieParser from "cookie-parser";
+ import cors from 'cors';
 
 
 
 const server = express();
 
 const port = process.env.PORT || 3000;
+const option = {
+    origin : ' http://localhost:5174/',
+    Credential : true,
+}
 
 server.use(express.json());
 server.use(express.urlencoded({extended : true}));
 server.use(cookieParser());
+server.use(cors());
 
 
 server.use("/user",userRouter);
